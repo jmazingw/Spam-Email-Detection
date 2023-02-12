072b7ede7b2d1d04ad8adb292194f6ed5af166d9
jmazingw <jdgonzales2@student.apc.edu.ph>
2023-02-13 00:04:45 +0800

    fixed code v8

--- Spam_detection.py
+++ Spam_detection.py
@@ -56,7 +56,7 @@ plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.title('Receiver operating characteristic example')
+plt.title('ROC Curve of the Spam Classifier')
 plt.legend(loc="lower right")
 plt.show()
 
